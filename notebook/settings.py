4a652d959a7fec7525930cb9b38a89fe54536d61
har-v <aliceharvey98@yahoo.co.uk>
2020-08-12 21:57:00 +0100

    Adding files

--- notebook/settings.py
+++ notebook/settings.py
@@ -132,24 +132,3 @@ STATICFILES_DIRS = [
 
 # My settings
 LOGIN_URL = '/users/login/'
-
-# Heroku settings
-if os.getcwd() == '/notebook':
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
-    
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    
-    # Allow all host readers
-    ALLOWED_HOSTS = ['*']
-    
-    # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-     
